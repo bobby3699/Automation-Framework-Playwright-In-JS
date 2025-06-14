cfe3eef8fbac230687bc720177403e6ff6bddc75
bobby3699 <51757558+bobby3699@users.noreply.github.com>
2025-06-14 19:55:35 +0530

    Delete src directory

--- src/tests/dashboard.spec.js
+++
@@ -1,7 +0,0 @@
-import { test, expect } from '../fixtures/base.fixture';
-
-test('Dashboard - Verify page elements and functionality', async ({ dashboardPage }) => {
-    await dashboardPage.adminClick();
-    await expect(dashboardPage.logoutButton).toBeVisible();
-})
-
